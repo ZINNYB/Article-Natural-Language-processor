876b5c8fbb8121ca41e16a53d6ccf27363518923
ZINNYB <32669452+ZINNYB@users.noreply.github.com>
2021-06-22 21:55:36 +0100

    fix: Resolve errror of 0

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,13 +1,20 @@
-import { validate } from "webpack";
-
 function handleSubmit(event) {
   event.preventDefault();
   // check what text was put into the form field
   let formText = document.getElementById("name").value;
   Client.checkForName(formText);
-
   console.log("::: Form Submitted :::");
-  fetch("http://localhost:8081/test")
+  fetch("http://localhost:8081/valid", {
+    method: "POST",
+    body: JSON.stringify({ content: formText }),
+    redirect: "follow",
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
     .then((res) => res.json())
     .then(function (res) {
       document.getElementById("results").innerHTML = res.message;
@@ -16,7 +23,6 @@ function handleSubmit(event) {
 // const nameValue = document.getElementById("name").value;
 // // form data
 const formdata = document.getElementById("name").value;
-
 async function userInput(url = "") {
   await fetch(url, {
     method: "POST",
@@ -30,13 +36,11 @@ async function userInput(url = "") {
     },
   });
   try {
-        const newData = await res.json();
-        console.log("response from /postData", newData);
-        document.getElementById('results').innerHTML = newData.message
-    } catch (error) {
-        console.log("error", error);
-    }
+    const newData = await res.json();
+    console.log("response from /postData", newData);
+    document.getElementById("results").innerHTML = newData.message;
+  } catch (error) {
+    console.log("error", error);
+  }
 }
-
-userInput("http://localhost:8081/valid");
 export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -10,61 +10,62 @@ var cors = require("cors");
 const url = `https://api.meaningcloud.com/lang-4.0/${Api_Key}`;
 const mockAPIResponse = require("./mockAPI.js");
 const { FormData } = require("formdata-node");
-const { inspector } = require('inspector');
-const { application } = require('express');
-
+const { inspector } = require("inspector");
+const { application } = require("express");
 // Instantiating
 const app = express();
 app.use(cors());
 // to use json
 // var jsonParser = bodyParser.json();
-
 // // create application/x-www-form-urlencoded parser
 // var urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 app.use(express.static("dist"));
-
 // get endpoint
 app.get("/", function (req, res) {
   res.sendFile(path.resolve("dist/index.html"));
 });
-
 app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
 });
-app.get("/valid", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.resolve("dist/index.html"));
 });
 app.post("/valid", function (req, res) {
   console.log(req.body);
-const nameData = req.body.content
-fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${Api_Key}&url=${nameData}&lang=en`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/JSON",
-            }
-        }).then((response) => {
-            return response.json();
-        }).then((data) => {
-            console.log(data);
-            res.send({
-                subjectivity: data.subjectivity,
-                agreement: data.agreement,
-                confidence: data.confidence,
-                irony: data.irony,
-                scoreTag: data.scoreTag
-            })
-        });
-})
+  const nameData = req.body.content;
 
-
+  fetch(
+    `https://api.meaningcloud.com/sentiment-2.1?key=${Api_Key}&url=${nameData}&lang=en`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/JSON",
+      },
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.send({
+        subjectivity: data.subjectivity,
+        agreement: data.agreement,
+        confidence: data.confidence,
+        irony: data.irony,
+        scoreTag: data.scoreTag,
+      });
+    });
+});
 app.listen(8081, function () {
   console.log("Example app listening on port 8081!");
 });
-
 /** Solution to the challenge
  * Make the api work in the server side
  * Make it take the value of the form input
