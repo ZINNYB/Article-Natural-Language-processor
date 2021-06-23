1c06f487c36c5c0468b43bb49339595d00994cd8
ZINNYB <32669452+ZINNYB@users.noreply.github.com>
2021-06-23 13:24:50 +0100

    further refactor code

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,36 +1,29 @@
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
   // check what text was put into the form field
   let formText = document.getElementById("name").value;
   Client.checkForName(formText);
   console.log("::: Form Submitted :::");
-
-// form data
-const formdata = document.getElementById("name").value;
-async function userInput(url = "") {
-  await fetch("http://localhost:8081/valid"
-  , {
+  fetch("http://localhost:8081/valid", {
     method: "POST",
-    body: JSON.stringify({ content: formdata }),
+    body: JSON.stringify({ content: formText }),
     redirect: "follow",
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache",
     credentials: "same-origin",
     headers: {
       "Content-Type": "application/json",
-    },
-  });
-  try {
-    const newData = await res.json()
-    .then(res => {
-      document.getElementById('score_tag').innerHTML = `score_tag: $(res.score_tag)`;
+    }
+  })
+    .then((res) => res.json())
+    .then(function (res) {
+      // const newData = await res.json();
+      console.log("response from /postData", res);
+      document.getElementById('score_tag').innerHTML = `score_tag: ${res.scoreTag}`;
       document.getElementById('agreement').innerHTML = `Agreement: ${res.agreement}`;
-      document.getElementById('subjectivity').innerHTML = `Subjectivity: ${res.subjectivity}`;
+      document.getElementById('sensitivity').innerHTML = `Subjectivity: ${res.subjectivity}`;
       document.getElementById('confidence').innerHTML = `Confidence: ${res.confidence}`;
       document.getElementById('irony').innerHTML = `Irony: ${res.irony}`;
-  });  } catch (error) {
-    console.log("error", error);
-  }
-}
+    });
 }
 export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -2,12 +2,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 var path = require("path");
 const express = require("express");
-
 const Api_Key = process.env.API_KEY;
 const fetch = require("node-fetch");
 var bodyParser = require("body-parser");
 var cors = require("cors");
-
 // other resources
 const url = `https://api.meaningcloud.com/lang-4.0/${Api_Key}`;
 const mockAPIResponse = require("./mockAPI.js");
@@ -25,38 +23,17 @@ app.use(
 app.use(bodyParser.json());
 app.use(express.static("dist"));
 // get endpoint
-app.get("/", function (req, res) {
-  res.sendFile(path.resolve("dist/index.html"));
-});
 app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
 });
 app.get("/", function (req, res) {
   res.sendFile(path.resolve("dist/index.html"));
 });
-
-
-
 app.post("/valid", function (req, res) {
   const nameData = req.body.content;
-  // Form data
-  const formdata = new FormData();
-  formdata.append("key", `${Api_Key}`);
-  formdata.append("txt", `${nameData}`);
-  formdata.append("lang", "en");  // 2-letter code, like en es fr ...
-
-  const requestOptions = {
-    method: 'POST',
-    body: formdata,
-    redirect: 'follow',
-    headers: {
-      "Content-Type": "application/JSON",
-    },
-  };
-  console.log(req.body);
-console.log(`https://api.meaningcloud.com/sentiment-2.1?`,requestOptions);
-  fetch(
-    `https://api.meaningcloud.com/sentiment-2.1?`, requestOptions)
+  console.log('nameData ======> ', nameData);
+  // console.log(`https://api.meaningcloud.com/sentiment-2.1?key=${Api_Key}&url=${nameData}&lang=en`);
+  fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${Api_Key}&url=${nameData}&lang=en`)
     .then((response) => {
       return response.json();
     })
@@ -67,7 +44,7 @@ console.log(`https://api.meaningcloud.com/sentiment-2.1?`,requestOptions);
         agreement: data.agreement,
         confidence: data.confidence,
         irony: data.irony,
-        scoreTag: data.scoreTag,
+        scoreTag: data.score_tag,
       });
     });
 });
