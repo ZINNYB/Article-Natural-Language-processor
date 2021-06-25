b3eaa2ef4cf5b7e36bf0858f26c226a69f9045ed
ZINNYB <32669452+ZINNYB@users.noreply.github.com>
2021-06-25 17:57:51 +0100

    Add a get route to /valid

--- src/server/index.js
+++ src/server/index.js
@@ -28,12 +28,13 @@ app.get("/test", function (req, res) {
 app.get("/", function (req, res) {
   res.sendFile(path.resolve("dist/index.html"));
 });
+app.get("/valid", function (req, res) {
+  res.sendFile(path.resolve("dist/index.html"));
+});
 app.post("/valid", function (req, res) {
   const nameData = req.body.content;
   console.log("nameData ======> ", nameData);
-  console.log(
-    `https://api.meaningcloud.com/sentiment-2.1?key=${Api_Key}&url=${nameData}&lang=en`
-  );
+
   fetch(
     `https://api.meaningcloud.com/sentiment-2.1?key=${Api_Key}&url=${nameData}&lang=en`
   )
@@ -54,10 +55,3 @@ app.post("/valid", function (req, res) {
 app.listen(8081, function () {
   console.log("Example app listening on port 8081!");
 });
-/** Solution to the challenge
- * Make the api work in the server side
- * Make it take the value of the form input
- * Make the api produce results based on the value of the form input
- *
- *
- */
